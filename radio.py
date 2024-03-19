87f140762f50c391382446d4c6d9e5a2fb8b5682
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2024-03-19 15:16:22 +0100

    Update radio.py
    Kopie vom Raspberry

--- radio.py
+++ radio.py
@@ -4,10 +4,11 @@ import time
 import os
 from threading import Thread
 os.system("mocp -S")
-os.system("mocp --volume=100")
+os.system("mocp --volume=70")
 clk = 17
 dt = 18
 counter = 0
+cur_counter = 0
 max_counter = 40
 activplayer = 0
 befehl = "mocp --playit "
@@ -52,22 +53,26 @@ def senderwahl():
 sendersuche = Thread(target=senderwahl)
 #mpplayer = Thread(target=play)
 sendersuche.start()
-
-# Sender aufwählen und abspielen
 while True:
-	mpplayer = Thread(target=play)
-	#print (counter)
-	frequenz = int((counter/max_counter*18)+88)
-	senderID = int((frequenz-88)/2)
-	#print (counter, frequenz, senderID)
-	#sleep(5)
-	print (activplayer)
-	if (frequenz % 2) and activplayer == 0:
-		#print (activplayer)
-		terminal = befehl + sender[senderID]
-		mpplayer.start()
-		activplayer = 1
-		sleep(1)
-	if not(frequenz % 2):
-		os.system("mocp --stop")
-		activplayer = 0
+	# print(counter, cur_counter)
+	# Sender aufwählen und abspielen
+	if counter < 0:
+		counter = 0
+	while cur_counter != counter:
+		mpplayer = Thread(target=play)
+		#print (counter)
+		cur_counter = counter
+		frequenz = int((counter/max_counter*18)+88)
+		senderID = int((frequenz-88)/2)
+		#print (counter, frequenz, senderID)
+		#sleep(5)
+		#print (activplayer, counter, frequenz)
+		if (frequenz % 2) and activplayer == 0:
+			#print (activplayer)
+			terminal = befehl + sender[senderID]
+			mpplayer.start()
+			activplayer = 1
+			#sleep(1)
+		if not(frequenz % 2):
+			os.system("mocp --stop")
+			activplayer = 0
