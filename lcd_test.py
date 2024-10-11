ea731f4845664877505c79a9271173237ed6771b
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2024-10-11 13:42:09 +0200

    Create lcd_test.py
    Erstellen der Datei

---
+++ lcd_test.py
@@ -0,0 +1,17 @@
+import drivers
+import sleep
+display = drivers.Lcd(0x27)
+try:
+  while True:
+    print("look at display")
+    display.lcd_display_string("Demo line 1", 1)
+    display.lcd_display_string("Demo line 2", 2)
+    display.lcd_display_string("Demo line 3", 3)
+    display.lcd_display_string("Demo line 4", 4)
+    time.sleep(2) # Give time for the message to be read
+    display.lcd_clear() # Clear the display of any data
+    time.sleep(2) # Give time for the message to be read
+except KeyboardInterrupt:
+  # exit on KeyboardInterrupt (when you press ctrl+c)
+  print("exit")
+  display.lcd_clear()
