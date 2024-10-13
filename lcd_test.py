08a009e9924deffa306dfd4ad5eebf08b7269c77
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2024-10-13 10:10:36 +0200

    Update lcd_test.py
    Uhrzeit

--- lcd_test.py
+++ lcd_test.py
@@ -6,8 +6,11 @@ try:
     print("look at display")
     display.lcd_display_string("Demo line 1", 1)
     display.lcd_display_string("Demo line 2", 2)
-    display.lcd_display_string("Demo line 3", 3)
-    display.lcd_display_string("Demo line 4", 4)
+    display.lcd_clear() # Clear the display of any data
+    time.sleep(2)
+    zeit = time.ctime()
+    display.lcd_display_string(zeit[11:16], 1)
+    display.lcd_display_string(zeit[20:24], 2)
     time.sleep(2) # Give time for the message to be read
     display.lcd_clear() # Clear the display of any data
     time.sleep(2) # Give time for the message to be read
