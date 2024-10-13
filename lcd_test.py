3f53b927cd991b37f6023d5f04ef497c3f5c2036
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2024-10-13 10:12:25 +0200

    Update lcd_test.py

--- lcd_test.py
+++ lcd_test.py
@@ -6,14 +6,13 @@ try:
     print("look at display")
     display.lcd_display_string("Demo line 1", 1)
     display.lcd_display_string("Demo line 2", 2)
-    display.lcd_clear() # Clear the display of any data
     time.sleep(2)
+    display.lcd_clear() # Clear the display of any data
     zeit = time.ctime()
     display.lcd_display_string(zeit[11:16], 1)
     display.lcd_display_string(zeit[20:24], 2)
-    time.sleep(2) # Give time for the message to be read
+    time.sleep(5) # Give time for the message to be read
     display.lcd_clear() # Clear the display of any data
-    time.sleep(2) # Give time for the message to be read
 except KeyboardInterrupt:
   # exit on KeyboardInterrupt (when you press ctrl+c)
   print("exit")
