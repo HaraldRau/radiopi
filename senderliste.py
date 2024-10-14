51e215c2f2406e979fafd41fa55a91e6f24570f2
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2024-10-14 15:18:13 +0200

    Update senderliste.py

--- senderliste.py
+++ senderliste.py
@@ -1,3 +1,8 @@
 def sender():
-  sender = [["01","Radio Nora","http://sc2.radiocaroline.net:10558/"],["Nr","Sender","Sender-URL"]]
+  sender = 
+  {
+    '01':["Radio Nora","http://sc2.radiocaroline.net:10558/"],
+    '02':["Sender","Sender-URL"]
+  
+  }
   return sender
