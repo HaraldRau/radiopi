f693b5d012468b512f8366cd4a4bb3c2c6ab3227
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2024-10-30 18:36:37 +0100

    Update senderliste.py
    Radioliste aktualisiert

--- senderliste.py
+++ senderliste.py
@@ -1,8 +1,12 @@
-def sender():
+def sender(id):
   sender = 
-  {
-    '01':["Radio Nora","http://sc2.radiocaroline.net:10558/"],
-    '02':["Sender","Sender-URL"]
-  
-  }
-  return sender
+  [
+    ["Radio Caroline","http://sc2.radiocaroline.net:10558/"],
+    ["70-iger","http://stream.laut.fm/1-hits70s"],
+    ["RSA Beatles","http://streams.rsa-sachsen.de/rsa-beatles/mp3-192/mediaplayerrsa"],
+    ["RSA Oldies","http://streams.rsa-sachsen.de/rsa-oldies/mp3-192/mediaplayerrsa"],
+    ["Oldies","http://laut.fm/oldies"],
+    ["Beat Goes on","http://stream.laut.fm/the-beat-goes-on"],
+    ["Northern Soul","http://stream.laut.fm/northernsoul"]
+  ]
+  return sender[id]
