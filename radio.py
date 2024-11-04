bb63f2017a1fd668f52dbbfb5e7790f4ae3ed418
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2024-11-04 13:17:03 +0100

    Update radio.py

--- radio.py
+++ radio.py
@@ -44,19 +44,19 @@ def senderwahl():
 			clkLastState = clkState
 			if GPIO.input(sw) == 0:
 				print(GPIO.input(sw))
-				display.lcd_display_string("--Setup--", 2)
-				time.sleep(2)
-				display.lcd_display_string("--Restart--", 2)
+				display.lcd_display_string("---Setup---", 2)
 				time.sleep(2)
 				if GPIO.input(sw) == 0:
+					display.lcd_display_string("--Restart--", 2)
+					time.sleep(2)
 					print("os.system('sudo /etc/init.d/radiopi restart')")
-				display.lcd_display_string("--Update--", 2)
-				time.sleep(2)
 				if GPIO.input(sw) == 0:
+					display.lcd_display_string("--Update--", 2)
+					time.sleep(2)
 					print("os.system('git pull https://github.com/HaraldRau/radiopi main')")
-				display.lcd_display_string("--Halt--", 2)
-				time.sleep(2)
 				if GPIO.input(sw) == 0:
+					display.lcd_display_string("---Halt---", 2)
+					time.sleep(2)
 					print("os.system('sudo halt')")
 				
 	finally:
