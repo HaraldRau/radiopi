76fd5e0b831b6d9a11d063f78101b52af80ae970
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2024-12-10 17:30:48 +0100

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -2,15 +2,16 @@
 # /usr/local/bin/startscript.sh
 sudo iwconfig wlan0 power off
 
-while ! ( ifconfig wlan0 | grep "192.168.178.44" )
+while ! ( ifconfig wlan0 | grep -q "192.168.178.44" )
 do
-    echo WLAN wird verbunden
+    echo "WLAN wird verbunden"
     # Warten auf Verbindung
+    sleep 1
 done
 
 sudo /etc/init.d/radiopi start
 sleep 5
-echo Radio ist gestartet
+echo "Radio ist gestartet"
 
 =======================================================================NEU============================================================================================
 
@@ -18,21 +19,21 @@ echo Radio ist gestartet
 # /usr/local/bin/startscript.sh
 
 # WLAN-Energiesparmodus deaktivieren (optional)
-sudo iwconfig wlan0 power off
+#sudo iwconfig wlan0 power off
 
 # Überprüfen, ob eine Verbindung zur gewünschten IP-Adresse besteht
-while ! (ifconfig wlan0 | grep -q "192.168.178.44")
-do
-    echo "WLAN wird verbunden..."
-    sleep 2
-done
+#while ! (ifconfig wlan0 | grep -q "192.168.178.44")
+#do
+#    echo "WLAN wird verbunden..."
+#    sleep 2
+#done
 
 # Sobald verbunden, den Radiopi-Dienst starten
-echo "WLAN-Verbindung steht, starte RadioPi..."
-sudo systemctl start radiopi
+#echo "WLAN-Verbindung steht, starte RadioPi..."
+#sudo systemctl start radiopi
 
 # Warten, um sicherzustellen, dass der Dienst läuft
-sleep 5
-sudo systemctl status radiopi
+#sleep 5
+#sudo systemctl status radiopi
 
-echo "RadioPi ist gestartet."
+#echo "RadioPi ist gestartet."
