1974b234cd98cf5dc285abdab60fb91db9aaad1a
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-01-28 15:11:09 +0100

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -7,6 +7,8 @@ do
     # Warten auf Verbindung
     sleep 1
 done
+git -C /home/hara/radiopi/ pull https://github.com/HaraldRau/radiopi.git
+sleep 1
 sudo /etc/init.d/radiopi start
 sleep 1
 echo "Radio ist gestartet"
