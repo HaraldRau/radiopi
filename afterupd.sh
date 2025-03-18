a379aee7b8b38e9175dada58e80e09db234d527e
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-03-18 15:51:58 +0100

    Update afterupd.sh

--- afterupd.sh
+++ afterupd.sh
@@ -3,5 +3,5 @@ echo "Aktionen nach git pull ausführen!"
 # sudo cp /home/hara/radiopi/config /root/.moc/config
 # sudo cp /home/hara/radiopi/editplaylist /root/.moc/editplaylist
 # sudo chmod +x /root/.moc/editplaylist
-sudo cp /home/hara/radiopi/startscript.sh /usr/local/bin/startscript.sh
+# sudo cp /home/hara/radiopi/startscript.sh /usr/local/bin/startscript.sh
 sudo /etc/init.d/radiopi restart&
