6c9e36169a7e5627f15f981187e5bf3aaeb4bde8
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-03-20 15:24:45 +0100

    Update afterupd.sh

--- afterupd.sh
+++ afterupd.sh
@@ -5,4 +5,5 @@ sudo cp /home/hara/radiopi/radiopi /etc/init.d/radiopi
 # sudo cp /home/hara/radiopi/editplaylist /root/.moc/editplaylist
 # sudo chmod +x /root/.moc/editplaylist
 # sudo cp /home/hara/radiopi/startscript.sh /usr/local/bin/startscript.sh
+sudo cp /home/hara/radiopi/scripte/webpi.service /etc/systemd/system/webpi.service
 sudo /etc/init.d/radiopi restart&
