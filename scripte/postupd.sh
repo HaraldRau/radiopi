7e981978232d3c7d79099334b4be2ce5656ce8cc
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-03-23 16:09:06 +0100

    Update postupd.sh

--- scripte/postupd.sh
+++ scripte/postupd.sh
@@ -1,8 +1,8 @@
 # !/bin/bash
 echo "Aktionen nach git pull ausführen!"
 # sudo cp /home/hara/radiopi/scripte/radiopi /etc/init.d/radiopi ENTFÄLLT
-# sudo cp /home/hara/radiopi/scripte/config /root/.moc/config
-# sudo cp /home/hara/radiopi/scripte/editplaylist /root/.moc/editplaylist&chmod +x /root/.moc/editplaylist
+# sudo cp /home/hara/radiopi/scripte/config /home/hara/.moc/config
+# sudo cp /home/hara/radiopi/scripte/editplaylist /home/hara/.moc/editplaylist&chmod +x /home/hara/.moc/editplaylist
 # sudo cp /home/hara/radiopi/scripte/startscript.sh /usr/local/bin/startscript.sh
 # sudo cp /home/hara/radiopi/scripte/webpi.service /etc/systemd/system/webpi.service&systemctl daemon-reload
 # sudo cp /home/hara/radiopi/scripte/radiopi.service /etc/systemd/system/radiopi.service&systemctl daemon-reload
