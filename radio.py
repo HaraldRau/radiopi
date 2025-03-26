a84a32cb80ef4a4d9db507696e777ee1303259b4
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-03-26 15:35:03 +0100

    Update radio.py

--- radio.py
+++ radio.py
@@ -42,7 +42,7 @@ def senderwahl():
 	clkLastState = GPIO.input(clk)
 	try:
 		while True:
-			time.sleep(0.1)
+			#time.sleep(0.1)
 			clkState = GPIO.input(clk)
 			dtState = GPIO.input(dt)
 			if clkState != clkLastState:
