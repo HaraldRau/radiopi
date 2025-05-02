4efd29538e79881f295af818f059fe0b133efb2d
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-05-02 09:47:32 +0200

    Update server.py

--- webinterface/server.py
+++ webinterface/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Erlaubte Befehle (Whitelist)
 ALLOWED_COMMANDS = {
     "mocp_play": "mocp -p",
-    "mocp_stop": "mocp -s",
+    "mocp_stop": "bash /home/hara/radiopi/scripte/pause",
     "mp3_list": "cat /home/hara/.moc/playlist.m3u|grep /home/",
     "mocp_liste": "bash /home/hara/radiopi/scripte/playliste",
     "mocp_titel": "mocp -Q%a&mocp -Q%t",
