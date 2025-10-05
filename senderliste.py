1dd6bf7cff0e1ed7a4d6aaf799804d37cf72d555
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-10-05 18:27:53 +0200

    Update senderliste.py

--- senderliste.py
+++ senderliste.py
@@ -3,7 +3,7 @@ def sender(id):
     ["Radio Caroline","http://sc2.radiocaroline.net:10558/"],
     ["Radio Nora","http://streams.norawebstreams.de/nora-oldies/mp3-192/"],
     ["RSA Oldies","http://streams.rsa-sachsen.de/rsa-oldies/mp3-192/mediaplayerrsa"],
-    ["Radio Bremen","https://www.radio.de/s/bremeneins"],
+    ["Radio Bremen","https://d131.rndfnk.com/ard/rb/bremeneins/live/mp3/128/stream.mp3?aggregator=radiolisten-de&cid=01FC1W5AX0V19NKBFPPE0A91GX&sid=33eco5Bcr07LSoVoP6QyJ9AzasK&token=tvOF2emYCvTTCiCzXKER6a51v_bQ8cBzMCz2_CbnAIk&tvf=4jqux1O4axhkMTMxLnJuZGZuay5jb20"],
     ["Hits 70s","http://stream.laut.fm/1-hits70s"],
     ["Our Generation","https://stream.laut.fm/oldies"],
     ["Beat Goes on","http://stream.laut.fm/the-beat-goes-on"],
