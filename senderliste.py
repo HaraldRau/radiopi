0f20484ffb438db345deba2078b1f4e6fb16abce
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2026-01-16 14:28:46 +0100

    Update sender list with new IDs and URLs

--- senderliste.py
+++ senderliste.py
@@ -1,13 +1,13 @@
 def sender(id):
   sender = [
-    ["RC01","http://sc2.radiocaroline.net:10558/"],
-    ["RN02","http://streams.norawebstreams.de/nora-oldies/mp3-192/"],
-    ["RO03","http://streams.rsa-sachsen.de/rsa-oldies/mp3-192/mediaplayerrsa"],
-    ["RB04","https://icecast.radiobremen.de/rb/bremeneins/live/mp3/128/stream.mp3"],
-    ["H705","http://stream.laut.fm/1-hits70s"],
-    ["OG06","https://stream.laut.fm/oldies"],
-    ["BG07","http://stream.laut.fm/the-beat-goes-on"],
-    ["NS08","http://stream.laut.fm/northernsoul"]
+    ["CA 1","http://sc2.radiocaroline.net:10558/"],
+    ["nO 2","http://streams.norawebstreams.de/nora-oldies/mp3-192/"],
+    ["SA 3","http://streams.rsa-sachsen.de/rsa-oldies/mp3-192/mediaplayerrsa"],
+    ["br 4","https://icecast.radiobremen.de/rb/bremeneins/live/mp3/128/stream.mp3"],
+    ["H7 5","http://stream.laut.fm/1-hits70s"],
+    ["OL 6","https://stream.laut.fm/oldies"],
+    ["GO 7","http://stream.laut.fm/the-beat-goes-on"],
+    ["SO 8","http://stream.laut.fm/northernsoul"]
   ]
   if id >= len(sender):
     id = len(sender)-1
