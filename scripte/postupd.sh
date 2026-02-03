8b35d2ad8be7917e12586507aa8042860c864e5f
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2026-02-03 14:38:07 +0100

    Uncomment radiopi.service copy command

--- scripte/postupd.sh
+++ scripte/postupd.sh
@@ -4,6 +4,6 @@ echo "Aktionen nach git pull ausführen!"
 # cp /home/hara/radiopi/scripte/editplaylist /home/hara/.moc/editplaylist
 # chmod +x /home/hara/.moc/editplaylist
 # sudo cp /home/hara/radiopi/scripte/webpi.service /etc/systemd/system/webpi.service
-# sudo cp /home/hara/radiopi/scripte/radiopi.service /etc/systemd/system/radiopi.service
+sudo cp /home/hara/radiopi/scripte/radiopi.service /etc/systemd/system/radiopi.service
 sudo systemctl restart webpi
 sudo systemctl restart radiopi
