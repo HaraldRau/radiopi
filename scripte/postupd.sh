b67bb0f2b9b72baad5e381d045b138fb9b79edbd
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2026-02-03 09:08:14 +0100

    Comment out config copy command in postupd.sh
    Comment out the configuration copy command in postupd.sh

--- scripte/postupd.sh
+++ scripte/postupd.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 echo "Aktionen nach git pull ausführen!"
-cp /home/hara/radiopi/scripte/config /home/hara/.moc/config
+# cp /home/hara/radiopi/scripte/config /home/hara/.moc/config
 # cp /home/hara/radiopi/scripte/editplaylist /home/hara/.moc/editplaylist
 # chmod +x /home/hara/.moc/editplaylist
 # sudo cp /home/hara/radiopi/scripte/webpi.service /etc/systemd/system/webpi.service
